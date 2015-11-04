40e3a39b5a6985396d55a06845b34bd65034eb79
Chris Cinelli <chris.cinelli@formativelearning.com>
2015-11-04 01:28:43 -0800

    setup controller $scope.dices and first rule

--- app/scripts/controllers/main.js
+++ app/scripts/controllers/main.js
@@ -8,12 +8,18 @@
  * Controller of the yahtzeeApp
  */
 angular.module('yahtzeeApp')
-  .controller('MainCtrl', function () {
-    // See: 'this' vs $scope in AngularJS controllers 
-    // http://stackoverflow.com/questions/11605917/this-vs-scope-in-angularjs-controllers
-    this.awesomeThings = [
-      'HTML5 Boilerplate',
-      'AngularJS',
-      'Karma'
+  .controller('MainCtrl', ['$scope', function ($scope) {
+    $scope.dices = [ 1, 2, 1, 4, 5, 6];
+
+    var rules = [
+      //https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/Array/Reduce
+      {name: 'one', compute: function(dices){return dices.reduce(function(sum, el){return sum + (el == 1 ? 1 : 0)}, 0)}},
     ];
-  });
+
+    //Just for debug:
+    for (var i in rules) {
+        var rule = rules[i];
+        console.log(rule.name, rule.compute($scope.dices));
+    }
+
+  }]);

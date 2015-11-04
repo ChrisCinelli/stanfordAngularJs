7ee656ee6b02af405d7135ee30e821617d6d71a6
Chris Cinelli <chris.cinelli@formativelearning.com>
2015-11-04 01:40:17 -0800

    Roll dices!

--- app/scripts/controllers/main.js
+++ app/scripts/controllers/main.js
@@ -11,6 +11,14 @@ angular.module('yahtzeeApp')
   .controller('MainCtrl', ['$scope', function ($scope) {
     $scope.dices = [ 1, 2, 1, 4, 5, 6];
 
+    $scope.roll = function(){
+        var dices = [];
+        for(var i = 1; i <= 6; i++){
+            dices.push(Math.floor(Math.random() * 6) + 1);
+        }
+        $scope.dices = dices;
+    }
+
     var rules = [
       //https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/Array/Reduce
       {name: 'one', compute: function(dices){return dices.reduce(function(sum, el){return sum + (el == 1 ? 1 : 0)}, 0)}},

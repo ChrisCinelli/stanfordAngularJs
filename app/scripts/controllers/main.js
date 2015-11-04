002575966b02542ee5e6b773b012c745e7f2f5c3
Chris Cinelli <chris.cinelli@formativelearning.com>
2015-11-04 01:11:30 -0800

    no message

--- app/scripts/controllers/main.js
+++ app/scripts/controllers/main.js
@@ -9,6 +9,8 @@
  */
 angular.module('yahtzeeApp')
   .controller('MainCtrl', function () {
+    // See: 'this' vs $scope in AngularJS controllers 
+    // http://stackoverflow.com/questions/11605917/this-vs-scope-in-angularjs-controllers
     this.awesomeThings = [
       'HTML5 Boilerplate',
       'AngularJS',

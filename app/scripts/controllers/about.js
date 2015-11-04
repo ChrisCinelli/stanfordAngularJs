e89617fdee911848ee6ed29c1ee7b98369e4877f
Chris Cinelli <chris.cinelli@formativelearning.com>
2015-11-04 00:57:53 -0800

    Yeomen generatored app

---
+++ app/scripts/controllers/about.js
@@ -0,0 +1,17 @@
+'use strict';
+
+/**
+ * @ngdoc function
+ * @name yahtzeeApp.controller:AboutCtrl
+ * @description
+ * # AboutCtrl
+ * Controller of the yahtzeeApp
+ */
+angular.module('yahtzeeApp')
+  .controller('AboutCtrl', function () {
+    this.awesomeThings = [
+      'HTML5 Boilerplate',
+      'AngularJS',
+      'Karma'
+    ];
+  });
---
+++ app/scripts/controllers/main.js
@@ -0,0 +1,17 @@
+'use strict';
+
+/**
+ * @ngdoc function
+ * @name yahtzeeApp.controller:MainCtrl
+ * @description
+ * # MainCtrl
+ * Controller of the yahtzeeApp
+ */
+angular.module('yahtzeeApp')
+  .controller('MainCtrl', function () {
+    this.awesomeThings = [
+      'HTML5 Boilerplate',
+      'AngularJS',
+      'Karma'
+    ];
+  });
